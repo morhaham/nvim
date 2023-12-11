21c411188d7647a2452f62eb61219f6ddcf5323c
Mor Haham <morh@codeoasis.com>
2023-12-11 11:16:30 +0200

    clean with lazy package manager and some plugs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,31 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
---
-vim.o.nohidden = true
+vim.g.mapleader = " "
+local options = {
+	termguicolors = true, -- Enable 24 bits color in terminal
+	guicursor = "a:block,i-ci-ve:ver25", -- Set the gui cursor to nothing for each mode
+	inccommand = "nosplit", -- Show effect of command incrementally while writing it (substitution)
+	showmode = false, -- Don't need to show mode
+	mouse = "a", -- Enable mouse in all mode
+	tabstop = 2, -- A tab is 2 spaces (display)
+	shiftwidth = 2, -- Indent to 2 spaces
+	softtabstop = 2, -- A tab is 2 spaces (insert mode)
+	expandtab = true, -- Use spaces instead of tab character
+	smartindent = true, -- Smart auto indent on new line
+	cursorline = true, -- Highlight the current line the cursor is on
+	clipboard = "unnamedplus", -- Use + register for yank, delete, change and put operation
+	hidden = true, -- Make sure hidden buffer are not unloaded
+	backup = false, -- Disable file backups
+	writebackup = false, -- Disable file backup on save
+	swapfile = false, -- Disable swap files
+	laststatus = 3, -- Single status line for the whole frame
+	signcolumn = "yes", -- Show the sign colum (for gitsigns)
+	number = true, -- Show line numbers
+	relativenumber = true, -- Use relative line numbers
+	scrolloff = 12, -- Keep at least 12 lines above the cursor.
+	wrap = false, -- Disable line wrap
+	colorcolumn = "80", -- Color the 120 character as a limit for line length
+}
+
+for opt, val in pairs(options) do 
+	vim[opt] = val
+end
+
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+    opts = {
+      ensure_installed = {
+        "stylua",
+      },
+    },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      require("mason-lspconfig").setup()
+    end,
+    opts = { ensure_installed = { "tsserver" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("lspconfig").tsserver.setup({})
+    end,
+  },
+}
